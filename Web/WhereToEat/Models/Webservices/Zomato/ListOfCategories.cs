0737a7cc19305187c60da19473fbf10722b5cd09
Juan Ramos <ptjuanramos@gmail.com>
2019-01-26 00:53:04 +0000

    Init commit for web application

---
+++ Web/WhereToEat/Models/Webservices/Zomato/ListOfCategories.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhereToEat.Models.Webservices.Zomato
+{
+    public class ListOfCategories
+    {
+    }
+}
