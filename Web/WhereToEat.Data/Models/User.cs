c4a7dd801f88ad8b1d70f6f031f3cb3e7ef910a1
Juan Ramos <ptjuanramos@gmail.com>
2019-01-27 23:07:16 +0000

    Different project(WhereToEat.Data) for a code-first database  creation. Implementation of FoodStats abstract class

---
+++ Web/WhereToEat.Data/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhereToEat.Data.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Salt { get; set; }
+    }
+}
---
+++ Web/WhereToEat/Models/Persistence/Group.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhereToEat.Models.Persistence
+{
+    [Table("Groups")]
+    public class Group
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public List<Guid> Admins { get; set; }
+
+        public List<Guid> Members { get; set; }
+    }
+}
